4e8c3c477ec3ffaa5237fe2e81ecc93a3fc26d94
Maimoon Hossain <ashikasitosh740@gmail.comm>
2023-11-11 21:12:07 +0600

    correction

--- script.js
+++ script.js
@@ -1,38 +1,10 @@
-let tl = gsap.timeline();
-
-tl.from('#nav', {
-  opacity: 0,
-  delay: 0.3,
-});
-
-tl.from('#nav h1, #nav h4, #nav h3', {
-  y: -80,
-  opacity: 0,
-  duration: 0.8,
-  delay: 0.5,
-  stagger: 0.3,
-});
-
-tl.from('#left h1', {
-  x: -500,
-  opacity: 0,
-  duration: 0.8,
-  stagger: 0.3,
-});
-
-tl.from('#right img', {
-  scale: 0.5,
-  opacity: 0,
-  duration: 0.5,
-});
-
-gsap.from('#page2 .box', {
-  scale: 0,
-  opacity: 0,
-  duration: 1,
-  stagger: 0.3,
+gsap.to('#page1 h1', {
+  transform: 'translateX(-100%)',
+  fontWeight: '100',
   scrollTrigger: {
-    trigger: '#page2 .box',
+    trigger: '#page1',
     scroller: 'body',
+    scrub: 3,
+    pin: true,
   },
 });
